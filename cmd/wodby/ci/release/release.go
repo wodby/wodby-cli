d9956555d327695146790c70a1c004a28a86da5d
Chingis Sandanov <chingis@wodby.com>
2023-10-04 09:29:36 +0300

    Login to registry only if there's no custom tag

--- cmd/wodby/ci/release/release.go
+++ cmd/wodby/ci/release/release.go
@@ -95,9 +95,11 @@ var Cmd = &cobra.Command{
 		docker := docker.NewClient()
 		registry := config.BuildConfig.Registry
 
-		err = docker.Login(registry.Host, registry.Username, registry.Password)
-		if err != nil {
-			return err
+		if opts.tag != "" {
+			err = docker.Login(registry.Host, registry.Username, registry.Password)
+			if err != nil {
+				return err
+			}
 		}
 
 		for _, service := range services {

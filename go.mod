7139601b43d3f1467bde962b5f2f8c68066e170d
Chingis Sandanov <chingis@wodby.com>
2023-12-04 13:46:26 +0300

    Change gql client

--- go.mod
+++ go.mod
@@ -1,26 +1,31 @@
 module github.com/wodby/wodby-cli
 
-go 1.13
+go 1.21
+
+require (
+	github.com/hasura/go-graphql-client v0.10.1
+	github.com/pkg/errors v0.8.0
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.1
+	github.com/spf13/pflag v1.0.0
+	github.com/spf13/viper v1.0.2
+)
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v0.0.0-20171017181929-23c074d0eceb // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/machinebox/graphql v0.2.2
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/magiconair/properties v1.7.6 // indirect
-	github.com/matryer/is v1.3.0 // indirect
 	github.com/mitchellh/mapstructure v0.0.0-20180220230111-00c29f56e238 // indirect
 	github.com/pelletier/go-toml v1.1.0 // indirect
-	github.com/pkg/errors v0.8.0
-	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/afero v1.0.2 // indirect
 	github.com/spf13/cast v1.2.0 // indirect
-	github.com/spf13/cobra v0.0.1
 	github.com/spf13/jwalterweatherman v0.0.0-20180109140146-7c0cea34c8ec // indirect
-	github.com/spf13/pflag v1.0.0
-	github.com/spf13/viper v1.0.2
-	github.com/stretchr/testify v1.5.1 // indirect
 	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
--- pkg/api/client.go
+++ pkg/api/client.go
@@ -2,14 +2,29 @@ package api
 
 import (
 	"context"
-	"fmt"
+	"net/http"
 	"os"
 
-	"github.com/machinebox/graphql"
+	"github.com/hasura/go-graphql-client"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/wodby/wodby-cli/pkg/types"
 )
+
+type transport struct {
+	underlyingTransport http.RoundTripper
+	apiKey              string
+	accessToken         string
+}
+
+func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if t.apiKey != "" {
+		req.Header.Set("X-API-KEY", t.apiKey)
+	} else {
+		req.Header.Set("X-ACCESS-TOKEN", t.accessToken)
+	}
+	return t.underlyingTransport.RoundTrip(req)
+}
 
 type Client struct {
 	client *graphql.Client
@@ -21,102 +36,62 @@ func NewClient(config types.APIConfig) *Client {
 	if os.Getenv("DEBUG") != "" {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
+	httpClient := http.Client{Transport: &transport{underlyingTransport: http.DefaultTransport}}
 	return &Client{
-		client: graphql.NewClient(config.Endpoint),
+		client: graphql.NewClient(config.Endpoint, &httpClient),
 		config: config,
 		logger: logrus.WithField("logger", "client"),
 	}
 }
 
 func (c *Client) GetAppBuild(ctx context.Context, id int) (types.AppBuild, error) {
-	req, err := c.getAuthorizedRequest(APP_BUILD)
+	var query struct {
+		AppBuild types.AppBuild `graphql:"appBuild(id: $id)"`
+	}
+	variables := map[string]interface{}{"id": id}
+	err := c.client.Query(ctx, &query, variables)
 	if err != nil {
 		return types.AppBuild{}, errors.WithStack(err)
 	}
-	req.Var("id", id)
-
-	var respData struct {
-		AppBuild types.AppBuild `json:"appBuild"`
-	}
-	c.logger.Debugf("Exec get app build request [id: %d]", id)
-	if err := c.client.Run(ctx, req, &respData); err != nil {
-		c.logger.Debugf("%v", respData)
-		return types.AppBuild{}, errors.WithStack(err)
-	}
-
-	return respData.AppBuild, nil
+	return query.AppBuild, nil
 }
 
 func (c *Client) GetDockerRegistryCredentials(ctx context.Context, appBuildID int) (types.DockerRegistryCredentials, error) {
-	req, err := c.getAuthorizedRequest(DOCKER_REGISTRY_CREDENTIALS)
+	var query struct {
+		DockerRegistryCredentials types.DockerRegistryCredentials `graphql:"dockerRegistryCredentials(appBuildID: $appBuildID)"`
+	}
+	variables := map[string]interface{}{"appBuildID": appBuildID}
+	err := c.client.Query(ctx, &query, variables)
 	if err != nil {
 		return types.DockerRegistryCredentials{}, errors.WithStack(err)
 	}
-	req.Var("appBuildID", appBuildID)
-
-	var respData struct {
-		DockerRegistryCredentials types.DockerRegistryCredentials `json:"dockerRegistryCredentials"`
-	}
-
-	c.logger.Debugf("Exec get docker credentials request [app build id: %d]", appBuildID)
-	if err := c.client.Run(ctx, req, &respData); err != nil {
-		return types.DockerRegistryCredentials{}, errors.WithStack(err)
-	}
-
-	return respData.DockerRegistryCredentials, nil
+	return query.DockerRegistryCredentials, nil
 }
 
 func (c *Client) Deploy(ctx context.Context, input types.DeploymentInput) (types.AppDeployment, error) {
-	req, err := c.getAuthorizedRequest(DEPLOY)
+	var m struct {
+		deployFromCI types.AppDeployment `graphql:"deployFromCI(input: $input)"`
+	}
+	variables := map[string]interface{}{"input": input}
+
+	err := c.client.Mutate(ctx, &m, variables)
 	if err != nil {
 		return types.AppDeployment{}, errors.WithStack(err)
 	}
-	req.Var("input", input)
 
-	var respData struct {
-		AppDeployment types.AppDeployment `json:"deployFromCI"`
-	}
-
-	c.logger.Debugf("Exec deploy request [input: %v]", input)
-	if err := c.client.Run(ctx, req, &respData); err != nil {
-		fmt.Printf("%+v", respData)
-		return types.AppDeployment{}, errors.WithStack(err)
-	}
-
-	return respData.AppDeployment, nil
+	return m.deployFromCI, nil
 }
 
 func (c *Client) NewCIBuild(ctx context.Context, input types.NewBuildFromCIInput) (types.AppBuild, error) {
-	req, err := c.getAuthorizedRequest(NEW_CI_BUILD)
+	var m struct {
+		newBuildFromCI types.AppBuild `graphql:"newBuildFromCI(input: $input)"`
+	}
+	variables := map[string]interface{}{"input": input}
+
+	err := c.client.Mutate(ctx, &m, variables)
 	if err != nil {
 		return types.AppBuild{}, errors.WithStack(err)
 	}
-	req.Var("input", input)
 
-	var respData struct {
-		AppBuild types.AppBuild `json:"newBuildFromCI"`
-	}
-
-	c.logger.Debugf("Exec new CI build request [input: %v]", input)
-	if err := c.client.Run(ctx, req, &respData); err != nil {
-		return types.AppBuild{}, errors.WithStack(err)
-	}
-	if respData.AppBuild.ID == 0 {
-		fmt.Printf("%+v", respData)
-		return types.AppBuild{}, errors.New("App build hasn't been created")
-	}
-
-	return respData.AppBuild, nil
-}
-
-func (c *Client) getAuthorizedRequest(query string) (*graphql.Request, error) {
-	req := graphql.NewRequest(query)
-
-	if c.config.Key != "" {
-		req.Header.Set("X-API-KEY", c.config.Key)
-	} else {
-		req.Header.Set("X-ACCESS-TOKEN", c.config.AccessToken)
-	}
-
-	return req, nil
+	return m.newBuildFromCI, nil
 }
